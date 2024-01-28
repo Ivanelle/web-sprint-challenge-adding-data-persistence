7a234cc1380d9210c265a3d1b98b726587f74efa
Ivanelle Hunt <ivanellep@gmail.com>
2024-01-27 17:56:35 -0800

    finished all tables, starting on seeds

--- data/migrations/20240128010843_initial-migration.js
+++ data/migrations/20240128010843_initial-migration.js
@@ -15,9 +15,43 @@ exports.up = async function(knex) {
 
     .createTable('resources', table => {
         table.increments('resource_id')
+        table.string('resource_name', 128)
+            .notNullable()
+            .unique()
+        table.string('resource_description', 256)
     })
     .createTable('tasks', table => {
         table.increments('task_id')
+        table.string('task_description', 128)
+            .notNullable()
+        table.string('task_notes', 256)
+        table.boolean('task_completed')
+            .defaultTo(0)
+        table.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('project_id')
+            .inTable('projects')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
+    })
+    .createTable('project_resources', table => {
+        table.increments('project_resources_id')
+        table.float('hours_allocated')
+            .defaultTo(0)
+        table.integer('project_id')
+            .unsigned()
+            .notNullable()
+            .references('project_id')
+            .inTable('projects')
+            .onDelete('RESTRICT')
+        table.integer('resource_id')
+            .unsigned()
+            .notNullable()
+            .references('resource_id')
+            .inTable('resources')
+            .onDelete('RESTRICT')
+
     })
 };
 
@@ -27,8 +61,9 @@ exports.up = async function(knex) {
  */
 exports.down = async function(knex) {
     await knex.schema
-        .dropTableIfExists()
-        .dropTableIfExists()
-        .dropTableIfExists()
+        .dropTableIfExists('project_resources')
+        .dropTableIfExists('tasks')
+        .dropTableIfExists('resources')
+        .dropTableIfExists('projects')
   
 };
