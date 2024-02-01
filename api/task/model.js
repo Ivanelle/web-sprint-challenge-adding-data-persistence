0365ec197d57abd4b5f566cb4de3b15574d887e9
Ivanelle Hunt <ivanellep@gmail.com>
2024-01-31 21:26:03 -0800

    tasks get endpoint is finished

--- api/task/model.js
+++ api/task/model.js
@@ -2,7 +2,16 @@
 const db = require('../../data/dbConfig')
 
 function getAllTasks() {
-   return db('tasks')
+   return db('tasks as t')
+    .join('projects as p', 't.project_id', 'p.project_id')
+    .select(
+        't.task_id',
+        't.task_description',
+        't.task_notes',
+        't.task_completed',
+        'p.project_name',
+        'p.project_description'
+    )
         .then(tasks => {
             const convertedTask = tasks.map(task => ({
                 ...task, 
